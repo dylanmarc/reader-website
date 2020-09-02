84677a9a044c4476525ef88a6f694c3f8153912f
dylanmarc <diloubilou@gmail.com>
2020-09-02 14:48:59 +1200

    Fixed utterance queueing, added visual indicators to show what is being said, added styling to page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
-import Reader from './components/Reader.js';
+
+
+function runReader(target) {
+  var txtInput = target.innerText;
+  var synth = window.speechSynthesis;
+  var voice = synth.getVoices().filter(function (voice) {
+    return voice.lang === 'en';
+  })[0];
+  
+  document.body.onkeyup = function (e) {
+    if (e.keyCode === 32) {
+      synth.cancel();
+    }
+  }
+
+  function runSpeak(txt) {
+    if(!synth.speaking && !synth.pending){
+    var toSpeak = new SpeechSynthesisUtterance(txt);
+    toSpeak.voice = voice;
+    toSpeak.pitch = 1;
+    toSpeak.rate = 1.5;
+    toSpeak.volume = 0.8;
+    target.className = 'reading';
+    synth.speak(toSpeak);
+    toSpeak.onend = function(){target.className = null;}
+    }
+  }
+
+  return (
+    runSpeak(txtInput)
+  )
+}
+
+// var selectedTxt = [];
+
+// function queueTxt(target){
+//   var element = {txt:target.innerText}
+//   if(selectedTxt.indexOf(element.txt) === -1){
+//     selectedTxt.push(element.txt);
+// }
+  
+// }
 
 class App extends Component {
   constructor(props) {
@@ -9,17 +50,30 @@ class App extends Component {
   }
 
   handleClick = (event) => {
-    Reader(event.target.innerText);
+    //queueTxt(event.target)
+    
+    runReader(event.target);
+    //console.log(selectedTxt);
   }
 
   render() {
     return (
-      <div class="main">
+      <div className="main">
+        <h1 onClick={this.handleClick}>Welcome to SpeakEasy v0.1</h1>
+
+        <h2 onClick={this.handleClick}>Click on any text to have it read aloud</h2>
+
         <p onClick={this.handleClick}>
-          In our basic Speech synthesiser demo (source), we first grab a reference to the SpeechSynthesis controller using window.speechSynthesis. After defining some necessary variables, we retrieve a list of the voices available using SpeechSynthesis.getVoices() and populate a select menu with them so the user can choose what voice they want.
+         SpeakEasy is a program designed to provide the impaired and 
+         illiterate population with a service that allows them to 
+         access information on the internet more effectively, 
+         efficiently and independently. 
         </p>
         <p onClick={this.handleClick}>
-          The SpeechSynthesisUtterance interface of the Web Speech API represents a speech request. It contains the content the speech service should read and information about how to read it (e.g. language, pitch and volume.)
+        The world is becoming more and more digitally focussed and 
+        we have to allow impaired and illiterate persons the same 
+        right and access to information online as the rest of the 
+        population.
         </p>
       </div>
     );
